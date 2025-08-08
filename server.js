c8bf90a0969eb6e260ababf986e1989f2f3b87e9
thisischaga <redocstar@gmail.com>
2025-08-08 21:43:22 +0200

    Mise à jour du backend

--- server.js
+++ server.js
@@ -17,7 +17,10 @@ connectDB();
 const app = express ();
 const port = 8000;
 
-app.use(cors())
+app.use(cors({
+    origin: 'https://dango-import.vercel.app',
+    credentials: true,
+}))
 app.use(express.json({limit: '125mb'}));
 
 // ADMIN PAR DEFAUT
