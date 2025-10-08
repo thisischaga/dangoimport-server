20333d6604b83f538ae60bde885e6b4c92cfb7da
thisischaga <redocstar@gmail.com>
2025-10-08 11:25:40 +0200

    Mise jour du système otp

--- server.js
+++ server.js
@@ -254,8 +254,8 @@ const startServer = async () => {
 
     const transporter = nodemailer.createTransport({
       host: 'smtp.hostinger.com',
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAIL_PASS,
